cdc0dd872efa3306d5a2f2aa8f848ac1a90f1638
delphifeel <delphifeel@gmail.com>
2015-02-09 17:15:39 +0200

    *

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+import json
+from django.db.models import Q
+from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+# Parse data like:
+# [{"item": "Помидоры", "amount": "1.5"}]
+# or
+# [{"item": "Соль"}]
+# or
+# [{"item": "Соль", "amount": "100"}, {"item": "Помидоры"}]
+from recipes.models import Ingredient, RecipeIngredients
+
+
+@csrf_exempt
+# TODO
+def api(request):
+    if request.method != 'POST':
+        raise Http404
+
+    body = request.body
+    if not body:
+        raise Http404
+
+    body = body.decode('utf8')
+    data = json.loads(body)
+    q = Q()
+    for obj in data:
+        item_name = obj['item']
+        if 'amount' in obj:
+            item_amount = float(obj['amount'])
+        else:
+            item_amount = 0
+
+        ing_list = Ingredient.objects.filter(name__contains=item_name)
+        q_ing = Q()
+        for ing in ing_list:
+            q_ing |= Q(ingredient=ing)
+
+        q_amount = Q()
+        if item_amount != 0:
+            q_amount = Q(amount__lte=item_amount)
+
+        q &= ((q_ing) & q_amount)
+
+    ri = RecipeIngredients.objects.filter(q)
+
+
+
+
+
+    return HttpResponse('Hello world', content_type='application/json; charset=UTF-8')
